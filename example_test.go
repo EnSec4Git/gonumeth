5444fef374256b15353c3796e5d4b3c1667a34c1
Yavor <yavorpap@abv.bg>
2014-02-17 13:14:32 +0200

    Updated Example tests to reduce printf precision

--- example_test.go
+++ example_test.go
@@ -22,58 +22,58 @@ var sinFunc SingleVarFunction = sin
 
 func ExampleNDifferentiateCentral() {
 	var diff = NDifferentiateCentral(sinFunc, math.Pi/2, derivH)
-	fmt.Println(diff)
-	// Output: -2.7755575615628914e-14
+	fmt.Printf("%.4e\n", diff)
+	// Output: -2.7756e-14
 }
 
 func ExampleNDifferentiateForward() {
 	var diff = NDifferentiateForward(sinFunc, math.Pi, derivH)
-	fmt.Println(diff)
-	// Output: -1.0000000000003382
+	fmt.Printf("%.4e\n", diff)
+	// Output: -1.0000e+00
 }
 
 func ExampleNDifferentiateBackward() {
 	var diff = NDifferentiateBackward(sinFunc, 0, derivH)
-	fmt.Println(diff)
-	// Output: 1.0000000000003
+	fmt.Printf("%.4e\n", diff)
+	// Output: 1.0000e+00
 }
 
 func ExampleNIntegrateGaussKronrodNonAdaptive() {
 	var res, _ = NIntegrateGaussKronrodNonAdaptive(sinFunc, 0,
 		math.Pi, 0.001, 0.001)
-	fmt.Println(res)
-	// Output: 2.0000000000008953
+	fmt.Printf("%.4e\n", res)
+	// Output: 2.0000e+00
 }
 
 func ExampleNIntegrateSimpsonAdaptive() {
 	var res, _ = NIntegrateSimpsonAdaptive(sinFunc, 0,
 		math.Pi/2, 0.001, 0.001)
-	fmt.Println(res)
-	// Output: 0.9999915654729925
+	fmt.Printf("%.4e\n", res)
+	// Output: 9.9999e-01
 }
 
 func ExampleNSimpleSolveBisection() {
 	var res = NSimpleSolveBisection(sinFunc, 3*math.Pi/4, maxIterations, defEpsilon)
-	fmt.Println(res)
-	// Output: 3.1415069901923447
+	fmt.Printf("%.4e\n", res)
+	// Output: 3.1415e+00
 }
 
 func ExampleNSimpleSolveNewton() {
 	var res = NSimpleSolveNewton(sinFunc, 5*math.Pi/4, maxIterations, defEpsilon)
-	fmt.Println(res)
-	// Output: 3.1415926409864197
+	fmt.Printf("%.4e\n", res)
+	// Output: 3.1416e+00
 }
 
 func ExampleNSimpleSolveHalley() {
 	var res = NSimpleSolveHalley(sinFunc, 7*math.Pi/4, maxIterations, defEpsilon)
-	fmt.Println(res)
-	// Output: 6.283185307175954
+	fmt.Printf("%.4e\n", res)
+	// Output: 6.2832e+00
 }
 
 func ExampleNSimpleSolveSecant() {
 	var res = NSimpleSolveSecant(sinFunc, 9*math.Pi/4, maxIterations, defEpsilon)
-	fmt.Println(res)
-	// Output: 6.283158738184373
+	fmt.Printf("%.4e\n", res)
+	// Output: 6.2832e+00
 }
 
 func test2d(x matrix.Matrix) (result matrix.Matrix) {
@@ -94,6 +94,6 @@ func ExampleNSolveSystemFixedPoint() {
 	x0.Set(0, 0, 0.6)
 	x0.Set(0, 1, 0.6)
 	var res = NSolveSystemFixedPoint(test2d, x0, maxIterations, defEpsilon)
-	fmt.Printf("%v %v\n", res.Get(0, 0), res.Get(0, 1))
-	//Output: 0.6462381444968512 0.6080337065319752
+	fmt.Printf("%.4e %.4e\n", res.Get(0, 0), res.Get(0, 1))
+	//Output: 6.4624e-01 6.0803e-01
 }
